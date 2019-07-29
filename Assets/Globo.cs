0130b1bad288a4de8d8d9a2534958009195c7c66
enrictena <enrictena96@gmail,com>
2019-07-29 12:58:29 +0200

    Globos_TestearMobil

---
+++ Assets/Globo.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Globo : MonoBehaviour
+{
+    private float m_Speed = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.up * Time.deltaTime * m_Speed;
+
+        if ((Input.touchCount > 0) && (Input.GetTouch(0).phase == TouchPhase.Began))
+        {
+            Ray raycast = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            RaycastHit raycastHit;
+            if (Physics.Raycast(raycast, out raycastHit))
+            {
+                //Debug.Log("Something Hit");
+                //if (raycastHit.collider.name == "Globo")
+                //{
+                //    Debug.Log("Soccer Ball clicked");
+                //}
+
+                //OR with Tag
+
+                if (raycastHit.collider.CompareTag("Globo"))
+                {
+                    Debug.Log("Globo clicked");
+                    Destroy(gameObject);
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "DestroyGlobo")
+            Destroy(gameObject);
+    }
+}
---
+++ Assets/Minijuego_Globos.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minijuego_Globos : MonoBehaviour
+{
+
+    private GameObject m_Spawn;
+    private GameObject m_Destroy;
+
+    public GameObject m_Globo;
+
+    private float m_TimePassed;
+    private float m_TimeToSpawn;
+    private Transform m_WhereToSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Spawn = GameObject.FindGameObjectWithTag("SpawnGlobo");
+        m_Destroy = GameObject.FindGameObjectWithTag("DestroyGlobo");
+
+        m_TimePassed = 0;
+        m_TimeToSpawn = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_TimePassed < 30)
+        {
+            m_TimePassed += Time.deltaTime;
+
+            if (m_TimeToSpawn > 1)
+            {
+                Instantiate(m_Globo, m_Spawn.transform.position + new Vector3(Random.Range(-7, 7), Random.Range(-1, 1), 0), Quaternion.identity);
+                m_TimeToSpawn = 0;
+            }
+            else
+                m_TimeToSpawn += Time.deltaTime;
+        }
+    }
+}
