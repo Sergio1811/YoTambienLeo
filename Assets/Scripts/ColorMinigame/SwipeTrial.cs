2dfb23a4e44f55f8ad21b0593eba20fd69a30567
Sergio Rodriguez <pekeno1811@gmail.com>
2019-08-01 10:08:38 +0200

    Resources & MiniGameColors

--- Assets/Scripts/ColorMinigame/SwipeTrial.cs
+++ Assets/Scripts/ColorMinigame/SwipeTrial.cs
@@ -5,22 +5,34 @@ using UnityEngine;
 public class SwipeTrial : MonoBehaviour
 {
     public GameObject ClonedSwipe;
-    
-    void Update()
+    bool m_CanClick = true;
+    void LateUpdate()
     {
+        print(m_CanClick);
         if ((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)|| Input.GetMouseButtonDown(0))
         {
-            
-          
-            Plane l_objPlane = new Plane(Camera.main.transform.forward * -1, this.transform.position);
 
-            Ray l_Ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            float l_rayDistance;
-            if (l_objPlane.Raycast(l_Ray, out l_rayDistance))
-               Instantiate(ClonedSwipe, l_Ray.GetPoint(l_rayDistance), ClonedSwipe.transform.rotation);
+            if (m_CanClick)
+            {
+                Plane l_objPlane = new Plane(Camera.main.transform.forward * -1, this.transform.position);
+
+                Ray l_Ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                float l_rayDistance;
+                if (l_objPlane.Raycast(l_Ray, out l_rayDistance))
+                    Instantiate(ClonedSwipe, l_Ray.GetPoint(l_rayDistance), ClonedSwipe.transform.rotation);
+            }
+
+           
         }
+
+        if(!m_CanClick && (Input.GetMouseButtonUp(0) || Input.GetTouch(0).phase == TouchPhase.Ended))
+        m_CanClick = true;
 
     }
 
+    public void ClickTesting()
+    {
+        m_CanClick = false;
+    }
     
 }
