9f7dff12596ccbc1f218361af64755ffe984c69e
Sergio Rodriguez <pekeno1811@gmail.com>
2019-08-10 12:18:39 +0200

    MenuConf

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,17 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     private static GameManager instance;
 
+    #region Configuracion
+    public string Idioma = "Español";
+    public Font TipoLetra;
+    public bool Mayus = true;
+    public bool Ayuda = true;
+    public bool Dumi = true;
+    public bool Articulo = false;
+    public int Repeticiones = 2;
+    public int Packs = 0;
+    #endregion
+
+    #region ScenesIndex
     public int PreparadosIndex;
     public int ListosIndex;
     public int YaIndex;
     public int GusanosIndex;
     public int BurbujasIndex;
     public int ColorIndex;
+    public int InicioIndex;
+    #endregion
+
+    #region ButtonUI
+    public Color m_BlackColor;
+    public Color m_PurpleColor;
+    public Color m_GrisColor;
+    public Color m_WhiteColor;
+
+    public Sprite ActiveButton;
+    public Sprite DesactivateButton;
+    #endregion
+
+    #region WordAdding(Cambiar de Sitio)
+    public string WordDifficulty;
+    public string Word;
+    #endregion
 
     void Awake()
     {
@@ -38,4 +67,10 @@ public class GameManager : MonoBehaviour
             return instance;
         }
     }
+
+    public void SaveWord(InputField input)
+    {
+        Word = input.text;
+    }
+
 }
--- Assets/Scripts/SceneManagement.cs
+++ Assets/Scripts/SceneManagement.cs
@@ -5,6 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class SceneManagement : MonoBehaviour
 {
+    public GameObject ConfCanvas;
+
+    public GameObject AñadirPalabraCanvas;
+
+    public GameObject EditarCanvas;
+
+    public GameObject MainMenuCanvas;
+
+    public GameObject ConfiguracionPestaña;
+
+    public GameObject[] AllCanvas;
+
+    public void InicioScene()
+     {
+        SceneManager.LoadScene(GameManager.Instance.InicioIndex);
+     }
      public void PreparadosScene()
      {
         SceneManager.LoadScene(GameManager.Instance.PreparadosIndex);
@@ -19,7 +35,23 @@ public class SceneManagement : MonoBehaviour
      }
      public void ConfScene()
      {
-
+        DisableAllCanvas();
+        ConfCanvas.SetActive(true);
+     }
+     public void ConfiguarcionScene()
+     {
+        DisableAllCanvas();
+        ConfiguracionPestaña.SetActive(true);
+     }
+     public void MainMenuScene()
+     {
+        DisableAllCanvas();
+        ConfCanvas.SetActive(true);
+     }
+     public void AddWordScene()
+     {
+        DisableAllCanvas();
+        ConfCanvas.SetActive(true);
      }
      public void MinijuegoGusanosScene()
      {
@@ -34,7 +66,19 @@ public class SceneManagement : MonoBehaviour
         SceneManager.LoadScene(GameManager.Instance.ColorIndex);
      }
      public void WebButton()
-    {
+     {
         Application.OpenURL("www.yotambienleo.com/recomendaciones");
+     }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void DisableAllCanvas()
+    {
+        for (int i = 0; i < AllCanvas.Length; i++)
+        {
+            AllCanvas[i].SetActive(false);
+        }
     }
 }
