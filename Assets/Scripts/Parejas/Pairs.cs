d4939ec0453e931d42da5c90c7ea8109e8f39800
ViodDevelopment <46675059+ViodDevelopment@users.noreply.github.com>
2019-10-07 09:49:59 +0200

    cambios

--- Assets/Scripts/Parejas/Pairs.cs
+++ Assets/Scripts/Parejas/Pairs.cs
@@ -102,7 +102,7 @@ public class Pairs : MonoBehaviour
 
             if (m_PieceClicked && (Input.GetMouseButtonUp(0) || (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)) && timer == 0)
             {
-                timer = 0.2f;
+                timer = 0.01f;
             }
         }
 
